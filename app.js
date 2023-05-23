ff7c07c80d67d5205915e4d8d6263fe135b39c66
OPaslavskiy <bankir.op@gmail.com>
2023-05-23 22:43:21 +0300

    add loger

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const logger = require("morgan");
 
 const cors = require("cors");
 require("dotenv").config();
@@ -13,6 +14,9 @@ const {
 
 const app = express();
 
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+
+app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
